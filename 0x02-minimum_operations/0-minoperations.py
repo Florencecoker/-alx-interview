3212e62e36aa6cb3291a2f4a991e9e020c5eb085
Florencecoker <damselpiccolo18@gmail.com>
2023-07-11 16:29:41 -0700

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python3
-
-
-''' A module that returns the minimum Operations it takes to
-    get to n characters.
-
-    Available operations:
-        copy
-        paste
-'''
+"""Module for task 0
+"""
 
 
 def minOperations(n):
-    '''
-    returns the minimum operations to get n H's
-    '''
-    min_operations = 0
+    """Calculates the fewest number of operations needed to result in
+    exactly n H characters in the file.
 
-    if n <= 1:
-        return min_operations
+    In a text file, there is a single character H. Your text editor can
+    execute only two operations in this file: Copy All and Paste characters
+    in the file.
 
-    for i in range(2, n + 1):
-        while n % i == 0:
-            min_operations += i
-            n /= i
+    Args:
+        n (int): The number of desired H characters.
 
-    return min_operations
-
-
-if __name__ == '__main__':
-    from random import randint
-    from time import time
-
-    start_time = time()
-
-    for i in range(10):
-        n = randint(2, 100)
-        print("Min # of operations to reach {} char: {}".
-              format(n, minOperations(n)))
-
- print(f'==> Program completed in {time() - start_time:.3f}s')
+    Returns:
+        int: The number of minimal operations needed to get n H characters
+    or 0 if it is impossible to achieve n.
+    """
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
